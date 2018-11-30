ae6a593ccf7e4f8d5e13efe60be56a0961e5a370
PhilippeDesu <35332328+PhilippeDesu@users.noreply.github.com>
2018-11-30 14:02:28 +0100

    Add files via upload

--- Webpage Dialearn/main.js
+++ Webpage Dialearn/main.js
@@ -1,13 +1,22 @@
-document.querySelector('#fach').addEventListener('click', select_doc);
- 
+'use strict'
 
-function select_doc(){
+function addFach()
+{
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyB-8gScjX7i5qYZYI_VTPOTOPQKW_peztE",
+    authDomain: "gawh-f81a5.firebaseapp.com",
+    databaseURL: "https://gawh-f81a5.firebaseio.com",
+    projectId: "gawh-f81a5",
+    storageBucket: "gawh-f81a5.appspot.com",
+    messagingSenderId: "251170516039"
+    };
+    firebase.initializeApp(config);
     
+    var x = document.getElementsByName("text_addFach")[0].value;
+    var coll = firebase.firestore().collection('GAWH');
+    
+    return coll.doc(x).set({}).then(function() {
+    alert("Document successfully written!");
+    });
 }
-  function berechne_BMI () {
-	var groesse = parseInt(document.getElementById("groesse").value),
-	    masse = parseInt(document.getElementById("masse").value),
-	    bmi = Math.round(100000 * masse / (groesse * groesse)) / 10;
-				
-	document.querySelector("output").textContent = bmi;
-  }
